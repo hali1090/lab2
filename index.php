7fdc9a697adcbbf003921627dc9c73289fa3e8a1
Hishaam <h.ali1090@outlook.com>
2021-01-28 17:25:45 +0000

    added front end to index

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
     include 'navigation.php';
     echo '<h1>Home Page</h1>';
     echo '<p>Welcome to the home page there isnt really much going on here</p>';
+    include 'hate_ball_frontend.html';
 ?>
